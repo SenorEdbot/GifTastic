8c5e67fc44745eedfcd4b0718ccde17973d8faf2
SenorEdbot <eddiefranco626@gmail.com>
2018-09-21 20:26:57 -0500

    gifs are on the page and they play / pause. only every other search term press for some reason

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -28,17 +28,39 @@ for (var i=0; i<topics.length; i++) {
 }
 
 $(document).on('click','.buttons', function(){
+    $('#gifDiv').empty();
     //store the button that was clicked in the SearchTerm
     searchTerm = $(this).attr('sport');
     API_KEY = 'wTP5c5YxtAnD8rfw628rBukV86aykclF';
 
-    //Returns 10 searchs with the search term
-    var queryURL = `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${searchTerm}&limit=10`
+    //Returns 10 searchs with the search term that are g or pg
+    var queryURL = `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${searchTerm}&limit=10&rating=pg`
 
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function(response){
         console.log(response);
-    })
-})
+        //For loop to loop through all the different objects in the data array
+        for (var i=0; i<response.data.length; i++) {
+            var newCard = $('<div class="card">').addClass("gifCards");
+            var cardImg = $('<img class="card-img-top">').attr('static-url',response.data[i].images.fixed_height_still.url).attr('dynamic-url',response.data[i].images.fixed_width.url).attr('isstatic','true');
+            cardImg.attr('src',cardImg.attr('static-url'));
+            var cardBody = $('<div class="card-body">');
+            var cardp = $('<p class="card-text">').text("Rating: " + response.data[i].rating);
+            cardBody.append(cardp);
+            newCard.append(cardImg,cardBody);
+            $('#gifDiv').append(newCard);
+        }
+    });
+
+    $(document).on('click', '.card-img-top', function(event){
+        if($(this).attr('isstatic')==='false'){
+            $(this).attr('src',$(this).attr('static-url'));
+            $(this).attr('isstatic', 'true');
+        } else {
+            $(this).attr('src',$(this).attr('dynamic-url'));
+            $(this).attr('isstatic', 'false');
+        };
+    });
+});
