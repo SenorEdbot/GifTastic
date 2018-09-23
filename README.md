df7344d753506d4c595858d986e35e8939be33df
SenorEdbot <eddiefranco626@gmail.com>
2018-09-22 21:46:15 -0500

    completed homework

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # GifTastic
+
+Sports Generator using Giphy API
+This web application will allow users to browse the top 10 trending gifs for whatever the sport they want. 
+
+    I have preloaded the page with 15 of the most common sports for the user's convenience. 
+    The gifs will be paused initially and the user can click on them to play them at their convenience. 
+    The user will be able to add sports to the search buttons whenever they want. 
+    If a user likes a gif they can 'Favorite' the gif and have it play on repeat on their page, even if they search something else.
+    The user can remove a favorite by clicking on the repeating gif. 
+    
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -21,6 +21,7 @@ var topics = [
     'weightlifting'
 ];
 var searchTerm;
+var favGifs = [];
 
 var generateButtons = function(){
     $('#topicButtons').empty();
@@ -48,17 +49,18 @@ $(document).on('click','.buttons', function(){
         //For loop to loop through all the different objects in the data array
         for (var i=0; i<response.data.length; i++) {
             var newCard = $('<div class="card">').addClass("gifCards");
-            var cardImg = $('<img class="card-img-top">').attr('static-url',response.data[i].images.fixed_height_still.url).attr('dynamic-url',response.data[i].images.fixed_width.url).attr('data-state','static').attr('isfav','false');
+            var cardImg = $('<img class="card-img-top">').attr('static-url',response.data[i].images.fixed_height_still.url).attr('dynamic-url',response.data[i].images.fixed_width.url).attr('data-state','static').attr('title', response.data[i].title);
             cardImg.attr('src',cardImg.attr('static-url'));
             var cardBody = $('<div class="card-body">');
             var cardp = $('<p class="card-text">').text("Rating: " + response.data[i].rating);
             var cardp2 = $('<p class="card-text">').text("Title: " + response.data[i].title);
-            cardBody.append(cardp2,cardp);
+            var cardButton = $('<button class="btn btn-info favButton">').text("Add Fav");
+            cardBody.append(cardp2,cardp, cardButton);
+            //cardBody.append(cardp2,cardp);
             newCard.append(cardImg,cardBody);
             $('#gifDiv').append(newCard);
         }
     });
-
 });
 //On Click Listener that will play and pause the gifs
 $(document).off('click', '.card-img-top').on('click', '.card-img-top', function (event) {
@@ -71,8 +73,31 @@ $(document).off('click', '.card-img-top').on('click', '.card-img-top', function 
         $(this).attr('data-state', 'static');
     };
 });
-$('#add-sport').on('click', function(){
+$('#add-sport').on('click', function () {
     event.preventDefault();
     topics.push($('#gif-input').val().trim());
     generateButtons();
+})
+$(document).on('click', '.favButton', function () {
+    $('#favHeader').show();
+    $('#favDiv').empty();
+    var newGifObject = {
+        dynamic: $(this).parent().parent().find("img").attr('dynamic-url'),
+        static: $(this).parent().parent().find("img").attr('static-url')
+    }
+    favGifs.push(newGifObject);
+    for (var j = 0; j < favGifs.length; j++) {
+        //loop to create the images
+        var newImg = $('<img>').attr('dynamic-url', favGifs[j].dynamic).attr('static-url', favGifs[j].static).addClass('favGif');
+        newImg.attr('src', newImg.attr('dynamic-url'));
+        $('#favDiv').append(newImg);
+    }
+})
+$(document).on('click', '.favGif', function () {
+    for (var k = 0; k < favGifs.length; k++) {
+        if (favGifs[k].dynamic === $(this).attr('dynamic-url')) {
+            favGifs.splice(k, 1);
+        }
+    }
+    $(this).remove();
 })
